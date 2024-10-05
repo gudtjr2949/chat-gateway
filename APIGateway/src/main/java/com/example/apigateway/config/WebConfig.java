436642cd3321e09ccc22877613654858572b19fd
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2024-10-05 23:24:01 +0900

    Feat: React CORS 경로 변경

--- APIGateway/src/main/java/com/example/apigateway/config/WebConfig.java
+++ APIGateway/src/main/java/com/example/apigateway/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://13.209.60.169:3000")
+                .allowedOrigins("http://122.42.35.24:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .exposedHeaders("location")
                 .allowedHeaders("*")
