65e62e5c814182c55ed239aabaa7a7ab2bf7e1d7
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2024-10-05 23:10:40 +0900

    First Commit

--- APIGateway/src/main/java/com/example/apigateway/config/GatewayConfig.java
+++ APIGateway/src/main/java/com/example/apigateway/config/GatewayConfig.java
@@ -8,13 +8,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
-    // /chat/*, /*/chat/**
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("chat-service", r -> r
                         .path("/chat/*", "/*/chat/**", "/ws/**") // WebSocket 경로
-                        .filters(f -> f.addRequestHeader("Upgrade", "websocket"))
+//                        .filters(f -> f.addRequestHeader("Upgrade", "websocket"))
                         .uri("lb://CHAT-SERVICE")) // Eureka에 등록된 서비스 이름
                 .build();
     }
